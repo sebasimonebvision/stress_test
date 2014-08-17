325cbffaa6ad53d842e35dc28fab4238a385db05
axisepm <axisepm@gmail.com>
2014-08-17 03:02:22 -0300

    Moved on with createNewUser method

--- stress script.rb
+++ stress script.rb
@@ -3,56 +3,52 @@ require 'yaml'
 require 'watir-webdriver'
 require 'rubygems'
 
-
-
-
 @log = Logger.new('activity.log')
 @log.level = Logger::INFO
 @log.info('started logging')
-sleep 2
+@config = YAML.load_file('config.yml')
+
 
 def getURL
-@config = YAML.load_file('config.yml')
 @baseURL = @config['baseURL']
 end
 
 def startBrowser
 @browser = Watir::Browser.new :firefox
+@browser.goto (@config['baseURL'])
 end  
 
-def doAdminLogin
-@browser.goto @baseURL
-@browser.text_field(:id => 'username').set(@config['adminUsername'])
-@browser.text_field(:id => 'pwd').set(@config['adminPassword'])
-@browser.link(:text => 'Log In').click
+def createNewUser
+startBrowser
+@browser.text_field(:id => 'username').when_present.set(@config['adminUsername'])
+@browser.text_field(:id => 'pwd').when_present.set(@config['adminPassword'])
+@browser.link(:href => "javascript:submitForm(document.getElementById('localLogin'), 'submit');").click
+@browser.link(:href => '/sf/sfmain/do/listProjectsAdmin').when_present.click
 end
 
-def createNewUser
-  
-end
+
+
+createNewUser
+
+=begin
 getURL
 startBrowser
 doAdminLogin 
 
-=begin
 tracker1 = Thread.new do
-  for a in 1..20
-  a=1
-  log.info ('Tracker 1 artifact created')
-  a = a+1 
+  getURL
+  startBrowser
+  doAdminLogin 
   end
-end
+
 
 tracker2 = Thread.new do 
-  for i in 1..20
-  i=1
-  log.info ('Tracker 2 artifact created')
-  i = i+1 
+  getURL
+  startBrowser
+  doAdminLogin 
   end
-
-end
-
 
 tracker1.join
 tracker2.join
+
 =end 
